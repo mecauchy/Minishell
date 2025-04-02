d740a33d05d4926c95e910c5a10891a0464562cb
Melissa Cauchy--Samake <mcauchy-@bess-f3r4s11.clusters.42paris.fr>
2025-04-02 17:48:52 +0200

    w_redirection

--- src/which_redirection.c
+++ src/which_redirection.c
@@ -6,7 +6,7 @@
 /*   By: mcauchy- <mcauchy-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 13:41:49 by mcauchy-          #+#    #+#             */
-/*   Updated: 2025/04/02 12:30:48 by mcauchy-         ###   ########.fr       */
+/*   Updated: 2025/04/02 13:43:58 by mcauchy-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,25 +77,6 @@ void	redirection_left_left(t_redir *current)
 	}
 }
 
-// revoir utilite
-// void	apply_redirection(t_redir *redir)
-// {
-// 	t_redir	*current;
-
-// 	current = redir;
-// 	while (current)
-// 	{
-// 		if (current->type == REDIR_OUT)
-// 			redirection_right(current);
-// 		else if (current->type == APPEND)
-// 			redirection_right_right(current);
-// 		else if (current->type == REDIR_IN)
-// 			redirection_left(current);
-// 		else if (current->type == HEREDOC)
-// 			redirection_left_left(current);
-// 		current = current->next;
-// 	}
-// }
 
 // remplacer char **cmd par cmd->token[] separes a l'aide du pipe
 void	help_redir(t_redir *redir)
